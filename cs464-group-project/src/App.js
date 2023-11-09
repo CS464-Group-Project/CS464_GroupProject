8b2352daa932e549ea2b36164af63c041f5aa411
David <davvan@pdx.edu>
2023-11-09 14:17:42 -0800

    Created routing with some pages

--- cs464-group-project/src/App.js
+++ cs464-group-project/src/App.js
@@ -1,5 +1,28 @@
+import { Home } from "./pages/Home";
+import { Team } from "./pages/Team";
+
+import { Link, Route, Routes } from "react-router-dom";
+
 function App() {
-    return null;
+  return (
+    <>
+      <nav className='navbar'>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/team'>Team</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/team' element={<Team />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
