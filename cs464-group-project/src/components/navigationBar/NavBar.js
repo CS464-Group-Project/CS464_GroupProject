b411bb4b5fc10eea3546edcae241e200a21a33d3
David <davvan@pdx.edu>
2023-11-11 00:29:15 -0800

    Edited Navbar using react bootstrap

--- cs464-group-project/src/components/navigationBar/NavBar.js
+++ cs464-group-project/src/components/navigationBar/NavBar.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { Navbar, Nav, NavItem, NavLink, Container } from 'reactstrap';
-import '../../style/NavigationBar.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../../style/NavigationBar.css";
 
 function NavBar() {
-    return (
+  return (
+    //https://react-bootstrap.netlify.app/docs/components/navbar
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Container className='nav-container'>
+        <Navbar.Brand href='/'>Football Stats</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='/team'>Team</Nav.Link>
+            <Nav.Link href='/player'>Player</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    /*
         <Navbar color='secondary'>
             <Container>
                 <Nav>
@@ -26,7 +43,8 @@ function NavBar() {
                 </Nav>
             </Container>
         </Navbar>
-    );
+        */
+  );
 }
 
 export default NavBar;
