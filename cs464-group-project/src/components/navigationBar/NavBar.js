1151d328959a944bfe1cf9a024c4ac81d184ad9f
David <davvan@pdx.edu>
2023-11-12 21:20:17 -0800

    Removed container

--- cs464-group-project/src/components/navigationBar/NavBar.js
+++ cs464-group-project/src/components/navigationBar/NavBar.js
@@ -8,42 +8,15 @@ function NavBar() {
   return (
     //https://react-bootstrap.netlify.app/docs/components/navbar
     <Navbar expand='lg' className='bg-body-tertiary'>
-      <Container className='nav-container'>
-        <Navbar.Brand href='/'>Football Stats</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link href='/team'>Team</Nav.Link>
-            <Nav.Link href='/player'>Player</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand href='/'>Football Stats</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='me-auto'>
+          <Nav.Link href='/team'>Team</Nav.Link>
+          <Nav.Link href='/player'>Player</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
-
-    /*
-        <Navbar color='secondary'>
-            <Container>
-                <Nav>
-                    <NavItem>
-                        <NavLink
-                            className='nav-link text-dark fw-bold fs-4'
-                            href='/'
-                        >
-                            Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className='nav-link text-dark fw-bold fs-4'
-                            href='/team'
-                        >
-                            Team
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Container>
-        </Navbar>
-        */
   );
 }
 
