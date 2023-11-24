bdc00e108a6bdd93567449777e8bd825bd520cb1
Tou Xiong <toufxiong83@gmail.com>
2023-11-24 01:04:07 -0800

    SCRUM-24: changed grid layout to page

--- cs464-group-project/src/pages/Team/IndividualTeam.jsx
+++ cs464-group-project/src/pages/Team/IndividualTeam.jsx
@@ -49,14 +49,11 @@ export function IndividualTeam() {
     return () => clearInterval(intervalId);
   }, [fanArtImages.length]);
 
-  // const index = Math.floor(Math.random() * fanArtImages.length);
-  // const randdomFanArt = fanArtImages[index];
-
   return (
     <>
       <h1 className='mt-4'>Welcome to {team.strAlternate} Page</h1>
-      <div className=' flex container-md mt-4'>
-        <div className='row' id='main-row'>
+      <div className=' flex mt-4'>
+        {/*<div className='row' id='main-row'>
           <div className='col-6-6 col-sm-6 col-md-4' id='team-logo'>
             <div className='row'>
               <div className='col-12'>
@@ -111,29 +108,94 @@ export function IndividualTeam() {
             <p>Stadium Capacity: {team.intStadiumCapacity}</p>
           </div>
         </div>
-        <div className='row' id='second-row'>
-          <div className='col-6-6 col-sm-6-6 col-lg-6'>
-            <div className='chart mt-4 '>
-              <TeamStats teamID={team.idTeam} />
-            </div>
-            <div className='team-schedule mt-4 p-4'>
-              <TeamsSchedule teamID={team.idTeam} />
+  */}
+        <div className='row' id='main-row'>
+          <div
+            className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-2'
+            id='links-logo'
+          >
+            <div className='row'>
+              <div className='col-6 col-sm-2 col-md-2 col-lg-2 col-xl-12'>
+                <a
+                  className='team-homepage'
+                  href={`https://${team.strWebsite}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <img
+                    className='image-fluid'
+                    id='team-logo'
+                    src={team.strTeamBadge}
+                    alt={`${team.strTeam} badge logo`}
+                    style={{ maxWidth: '100%', maxHeight: 'auto' }}
+                  />
+                </a>
+              </div>
+              <div className='col-12 col-sm-10 d-flex justify-content-evenly social-media-icons'>
+                <a
+                  href={`https://${team.strTwitter}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <a
+                  href={`https://${team.strFacebook}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a
+                  href={`https://${team.strYoutube}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+              </div>
             </div>
           </div>
-          <div className='col'>
-            <div className='row'></div>
-            <div className='col team-players mt-4 p-4'>
-              <TeamPlayers teamName={team.strTeam} />
+          <div className='col-sm-12 col-md-6 col-lg-6 col-xl-4' id='team-info'>
+            <div className='row'>
+              <div className='col-12 col-lg-12 chart mt-4 '>
+                <TeamStats teamID={team.idTeam} />
+              </div>
+              <div className='col mt-4 p-4' id='team-schedule'>
+                <TeamsSchedule teamID={team.idTeam} />
+              </div>
             </div>
-            <div className='col fan-art'>
-              <h3>Fan Art</h3>
-              <img
-                className='image-fluid transition-fade'
-                src={fanArtImages[currentImageIndex]}
-                alt={`${team.strTeam} - Fan Arts`}
-                style={{ maxWidth: '100%', maxHeight: 'auto' }}
-              />
+          </div>
+          <div className='col-12 col-md-6 col-lg-6 col-xl-4'>
+            <div className='row'>
+              <div
+                className='col-12 col-sm-12 col-md-12 col-lg-12 mt-4 p-4'
+                id='team-players'
+              >
+                <TeamPlayers teamName={team.strTeam} />
+              </div>
+              <div className='col' id='fan-art'>
+                <h3>Fan Art</h3>
+                <img
+                  className='image-fluid transition-fade'
+                  src={fanArtImages[currentImageIndex]}
+                  alt={`${team.strTeam} - Fan Arts`}
+                  style={{ maxWidth: '100%', maxHeight: 'auto' }}
+                />
+              </div>
             </div>
+          </div>
+          <div className='col-12 col-xl-2'>
+            <img
+              className='image-fluid'
+              src={team.strStadiumThumb}
+              alt={`${team.strTeam} stadium`}
+              style={{ maxWidth: '100%', height: 'auto' }}
+            />
+            <p>Country: {team.strCountry}</p>
+            <p>Formed Year: {team.intFormedYear}</p>
+            <p>Stadium: {team.strStadiumLocation}</p>
+            <p>Stadium Capacity: {team.intStadiumCapacity}</p>
           </div>
         </div>
       </div>
