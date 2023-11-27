cc45b97cf4d1489ead8e44c42a3766b99a729b19
lebenpdx <116242222+lebenpdx@users.noreply.github.com>
2023-11-26 19:56:18 -0800

    Contract history timeline

--- cs464-group-project/src/components/charts/ContractTimeline.js
+++ cs464-group-project/src/components/charts/ContractTimeline.js
@@ -1,9 +1,27 @@
-// contractTimeline.js
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 export const ContractTimeline = ({ contracts }) => {
   let chartData = [];
+
   if (contracts.length != []) {
     chartData = contracts.contracts.map((contracts) => ({
       team: contracts.strTeam,
@@ -11,43 +29,53 @@ export const ContractTimeline = ({ contracts }) => {
       endYear: contracts.strYearEnd,
     }));
 
-    let datasets = [
-      {
-        label: chartData.map((team) => team.team),
-        datasets: [
-          {
-            label: 'Years Played',
-            data: chartData.map((team) => ({
-              x: [team.startYear, team.endYear],
-            })),
-          },
-        ],
+    const minStartYear = Math.min(...chartData.map((team) => team.startYear));
+    const maxEndYear = Math.max(...chartData.map((team) => team.endYear));
 
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(75,192,192,0.6)',
-        hoverBorderColor: 'rgba(75,192,192,1)',
-      },
-    ];
-
+    let datasets = {
+      labels: chartData.map((team) => team.team),
+      datasets: [
+        {
+          label: 'Years Played',
+          data: chartData.map((team) => ({
+            x: [team.startYear, team.endYear],
+            y: team.team,
+          })),
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(75,192,192,0.6)',
+          hoverBorderColor: 'rgba(75,192,192,1)',
+        },
+      ],
+    };
     console.log(datasets);
+
+    const chartOptions = {
+      type: 'bar',
+      responsive: true,
+      indexAxis: 'y',
+      scales: {
+        x: {
+          min: minStartYear,
+          max: maxEndYear,
+          ticks: {
+            stepSize: 1,
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Timeline of contract history',
+        },
+      },
+    };
 
     return <div>{<Bar data={datasets} options={chartOptions} />}</div>;
   }
   return <div>Failed to load player contract timeline</div>;
-};
-
-const chartOptions = {
-  scales: {
-    x: {
-      type: 'linear',
-      position: 'bottom',
-      beginAtZero: true,
-    },
-    y: {
-      type: 'category',
-      position: 'left',
-    },
-  },
 };
