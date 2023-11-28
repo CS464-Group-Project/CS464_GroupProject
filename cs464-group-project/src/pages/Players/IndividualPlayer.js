699e582be37754ca78ff1ac78a2825805cc594bc
lebenpdx <116242222+lebenpdx@users.noreply.github.com>
2023-11-28 05:26:56 -0800

    New individual player layout

--- cs464-group-project/src/pages/Players/IndividualPlayer.js
+++ cs464-group-project/src/pages/Players/IndividualPlayer.js
@@ -55,116 +55,145 @@ export function IndividualPlayer() {
   };
 
   return (
-    <div className='container-md mt-4'>
+    <>
       {playerData ? (
-        <div className='row'>
-          {/* Left side with player image */}
-          <div className='col-md-6'>
-            <img
-              src={playerData.strThumb}
-              alt={`${playerData.strPlayer}`}
-              className='img-fluid'
-            />
-            {/*Social media links if they exist*/}
-            <div className='col d-flex mt-3 justify-content-evenly'>
-              {playerData.strTwitter && (
-                <a
-                  href={`https://${playerData.strTwitter}`}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <FontAwesomeIcon
-                    icon={faTwitter}
-                    style={{ fontSize: '2em' }}
-                  />
-                </a>
-              )}
-              {playerData.strInstagram && (
-                <a
-                  href={`https://${playerData.strInstagram}`}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <FontAwesomeIcon
-                    icon={faInstagram}
-                    style={{ fontSize: '2em' }}
-                  />
-                </a>
-              )}
-              {playerData.Youtube && (
-                <a
-                  href={`https://${playerData.strYoutube}`}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <FontAwesomeIcon
-                    icon={faYoutube}
-                    style={{ fontSize: '2em' }}
-                  />
-                </a>
-              )}
-            </div>
-          </div>
-
-          {/* Right side with player details */}
-          <div className='col-md-6'>
-            <div className='d-flex align-items-center'>
-              {playerData.strNumber && (
-                <h2>
-                  #{playerData.strNumber} {playerData.strPlayer}
-                </h2>
-              )}
-              {!playerData.strNumber && <h2>{playerData.strPlayer}</h2>}
-
-              {/* Team logo as clickable img next to player's name */}
-              {teamData && (
-                <img
-                  src={teamData.strTeamBadge}
-                  alt={`${teamData.strTeam} Logo`}
-                  className='img-fluid team-logo'
-                  style={{
-                    height: '2.5em',
-                    marginLeft: '10px',
-                    cursor: 'pointer',
-                  }}
-                  onClick={handleTeamClick}
-                  title={`Click to visit the ${teamData.strTeam} page`}
-                />
-              )}
-            </div>
-
-            {/* Other player details */}
+        <div>
+          <header className='container'>
             <div className='row'>
-              <div className='col-6'>
-                <p>Team: </p>
-                <p>Position: </p>
-                <p>Date of Birth: </p>
-                <p>Nationality:</p>
-
-                <p>Height: </p>
+              {/* Image on the left */}
+              <div className='col-md-4'>
+                <img
+                  src={playerData.strRender}
+                  alt={`{playerData.strPlayer} render`}
+                  className='img-fluid'
+                />
               </div>
-              <div className='col-6' style={{ textAlign: 'right' }}>
-                <p>{playerData.strTeam}</p>
-                <p>{playerData.strPosition}</p>
-                <p>{formatDate(playerData.dateBorn)}</p>
-                <p>{playerData.strNationality}</p>
-                <p>{playerData.strHeight}</p>
+
+              {/* Text in the middle (slightly elevated) */}
+              <div className='col-md-4 d-flex align-items-center'>
+                <h1 className='mt-3'>{playerData.strPlayer}</h1>
+              </div>
+
+              {/* Text on the bottom right */}
+              <div className='col-md-4 d-flex justify-content-end align-items-end'>
+                {playerData.strNumber && (
+                  <h1 className='mb-0'>#{playerData.strNumber}</h1>
+                )}
               </div>
             </div>
-          </div>
+          </header>
 
-          {/*Contract Timeline Chart component */}
-          <div className='col-md-12'>
-            <PlayerFormerTeams id={id} />
-          </div>
-          <div className='col-md-6'>{<PlayerHonours id={id} />}</div>
-          <div className='col-md-6'>
-            <PlayerFormerBadges id={id} />
+          <div className='container-md mt-4'>
+            <div className='row'>
+              {/* Right side with player details */}
+              <div className='col-md-6'>
+                <div className='d-flex align-items-center'>
+                  {playerData.strNumber && (
+                    <h2>
+                      #{playerData.strNumber} {playerData.strPlayer}
+                    </h2>
+                  )}
+                  {!playerData.strNumber && <h2>{playerData.strPlayer}</h2>}
+                </div>
+
+                {/* Other player details */}
+                <div className='row'>
+                  <div className='col-6'>
+                    <p>Team: </p>
+                    <p>Position: </p>
+                    <p>Date of Birth: </p>
+                    <p>Nationality:</p>
+
+                    <p>Height: </p>
+                  </div>
+                  <div className='col-6' style={{ textAlign: 'right' }}>
+                    <p>{playerData.strTeam}</p>
+                    <p>{playerData.strPosition}</p>
+                    <p>{formatDate(playerData.dateBorn)}</p>
+                    <p>{playerData.strNationality}</p>
+                    <p>{playerData.strHeight}</p>
+                  </div>
+                </div>
+
+                <div className='col d-flex mt-3 justify-content-evenly'>
+                  {/* Team logo as clickable img next to player's name */}
+                  {teamData && (
+                    <img
+                      src={teamData.strTeamBadge}
+                      alt={`${teamData.strTeam} Logo`}
+                      className='img-fluid team-logo'
+                      style={{
+                        height: '4em',
+                        marginLeft: '10px',
+                        cursor: 'pointer',
+                      }}
+                      onClick={handleTeamClick}
+                      title={`Click to visit the ${teamData.strTeam} page`}
+                    />
+                  )}
+
+                  {playerData.strTwitter && (
+                    <a
+                      href={`https://${playerData.strTwitter}`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      <FontAwesomeIcon
+                        icon={faTwitter}
+                        style={{ fontSize: '4em' }}
+                      />
+                    </a>
+                  )}
+                  {playerData.strInstagram && (
+                    <a
+                      href={`https://${playerData.strInstagram}`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      <FontAwesomeIcon
+                        icon={faInstagram}
+                        style={{ fontSize: '4em' }}
+                      />
+                    </a>
+                  )}
+                  {playerData.Youtube && (
+                    <a
+                      href={`https://${playerData.strYoutube}`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      <FontAwesomeIcon
+                        icon={faYoutube}
+                        style={{ fontSize: '4em' }}
+                      />
+                    </a>
+                  )}
+                </div>
+              </div>
+              {/* Left side with player image */}
+              <div className='col-md-6'>
+                <img
+                  src={playerData.strThumb}
+                  alt={`${playerData.strPlayer}`}
+                  className='img-fluid'
+                />
+                {/*Social media links if they exist*/}
+              </div>
+
+              {/*Contract Timeline Chart component */}
+              <div className='col-md-12'>
+                <PlayerFormerTeams id={id} />
+              </div>
+              <div className='col-md-6'>{<PlayerHonours id={id} />}</div>
+              <div className='col-md-6'>
+                <PlayerFormerBadges id={id} />
+              </div>
+            </div>
           </div>
         </div>
       ) : (
         <p>Loading...</p>
       )}
-    </div>
+    </>
   );
 }
