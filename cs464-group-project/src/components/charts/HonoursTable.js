19bd9a31c87cefe2fdae70db43c59d221b2c929e
lebenpdx <116242222+lebenpdx@users.noreply.github.com>
2023-11-29 06:39:58 -0800

    Added case for no honours

--- cs464-group-project/src/components/charts/HonoursTable.js
+++ cs464-group-project/src/components/charts/HonoursTable.js
@@ -4,7 +4,9 @@ import '../../style/Player.css';
 export const HonoursTable = ({ honours }) => {
   if (honours) {
     const data = honours.honours;
-
+    if (data === null) {
+      console.log('bad');
+    }
     return (
       <table className='honours-table'>
         <thead>
@@ -15,14 +17,21 @@ export const HonoursTable = ({ honours }) => {
           </tr>
         </thead>
         <tbody>
-          {data &&
+          {!data ? (
+            <tr>
+              <td colSpan={3} style={{ textAlign: 'center' }}>
+                No Career Honours
+              </td>
+            </tr>
+          ) : (
             data.map((honour) => (
               <tr key={honour.id}>
                 <td>{honour.strHonour}</td>
                 <td>{honour.strSeason}</td>
                 <td>{honour.strTeam}</td>
               </tr>
-            ))}
+            ))
+          )}
         </tbody>
       </table>
     );
