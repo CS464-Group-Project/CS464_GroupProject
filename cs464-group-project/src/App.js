860141571db7679f261179dbb424e73623c190c1
Tou Xiong <toufxiong83@gmail.com>
2023-11-30 21:16:07 -0800

    back to original

--- cs464-group-project/src/App.js
+++ cs464-group-project/src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Home2 } from './pages/Home_2';
 import { Team } from './pages/Team/TeamsMainPage';
 import { Player } from './pages/Players/PlayersMainPage';
 import { IndividualPlayer } from './pages/Players/IndividualPlayer';
@@ -15,7 +14,7 @@ function App() {
       <div className='App'>
         <NavBar />
         <Routes>
-          <Route path='/' element={<Home2 />} />
+          <Route path='/' element={<Home />} />
           <Route path='/teamsmainpage' element={<Team />} />
           <Route path='/player' element={<Player />} />
           <Route path='/individualteam' element={<IndividualTeam />} />
