95b9fbe3334ce9aa62e2a9945edbb9316d5aa4fd
Tou Xiong <toufxiong83@gmail.com>
2023-11-30 21:02:04 -0800

    Tou: mobile view style

--- cs464-group-project/src/App.js
+++ cs464-group-project/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Home2 } from './pages/Home_2';
 import { Team } from './pages/Team/TeamsMainPage';
 import { Player } from './pages/Players/PlayersMainPage';
 import { IndividualPlayer } from './pages/Players/IndividualPlayer';
@@ -14,7 +15,7 @@ function App() {
       <div className='App'>
         <NavBar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home2 />} />
           <Route path='/teamsmainpage' element={<Team />} />
           <Route path='/player' element={<Player />} />
           <Route path='/individualteam' element={<IndividualTeam />} />
