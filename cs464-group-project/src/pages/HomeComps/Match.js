7f030e89635567e2009f9a5f1388578867a2023d
David <davvan@pdx.edu>
2023-12-01 13:50:04 -0800

    changed mobile layout for matches

--- cs464-group-project/src/pages/HomeComps/Match.js
+++ cs464-group-project/src/pages/HomeComps/Match.js
@@ -4,26 +4,24 @@ function Match({ match }) {
   return (
     <li key={match.id} className='match-item'>
       <div className='team-names'>
-        <div className='team-info team-cell'>
+        <div className='match-home team-cell'>
           <img
             src={match.homeLogo}
             className='team-logo'
             alt={`${match.homeName} Logo`}
           ></img>
-          <div>{match.homeName}</div>
+          <div className='match-name'>{match.homeName}</div>
+          <div className='match-home-score'>{match.homeScore}</div>
         </div>
-        <div className='team-info team-cell'>
+        <div className='match-away team-cell'>
           <img
             src={match.awayLogo}
-            className='team-logo'
+            className='match-away-logo'
             alt={`${match.awayName} Logo`}
           ></img>
-          <div className='team-info'>{match.awayName}</div>
+          <div className='match-name'>{match.awayName}</div>
+          <div className='match-away-score'>{match.awayScore}</div>
         </div>
-      </div>
-      <div className='match-scores'>
-        <div className='team-info'>{match.homeScore}</div>
-        <div className='team-info'>{match.awayScore}</div>
       </div>
     </li>
   );
